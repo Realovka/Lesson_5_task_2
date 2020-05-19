0ef934dd1df455d8276e1e80d65989320f175404
Nastya <realovka@mail.ru>
2020-05-19 23:21:26 +0300

    2 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,53 +7,49 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Введите размерность массива");
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int[][] array = new int[n][n];
         int i = 0;
         int j = 0;
-        int a = 1;
-        int b=0;
+        int a = 1; //счетчик
+        int g=n-1; //переменная для шага
+        int p=0; // переменная для шага
 
+        do {
+        while (j<g) {
+            array[i][j] = a;
+            a++;
+            j++;
+        }
+        while (i<g){
+            array[i][j]=a;
+            a++;
+            i++;
+        }
+        while (j>p){
+            array[i][j]=a;
+            a++;
+            j--;
+        }
+        g--;
+        p++;
+        while (i>p){
+            array[i][j]=a;
+            a++;
+            i--;
+        }
 
-        for (i = 0; i < array.length; i++) {
-            for (j = 0; j < array.length; j++) {
-                while (b<2*n) {
+        } while (a!=n*n);
 
-                    for (j = 0; j < array.length; j++) {
-                        i = 0;
-                        array[i][j] = a;
-                        a++;
-                    }
-                    b++;
-                    for (i = 1; i < array.length; i++) {
-                        j = array.length - 1;
-                        array[i][j] = a;
-                        a++;
-                    }
-                    b++;
-                    for (j = array.length-2; j >= 0; j--) {
-                        i = array.length-1;
-                        array[i][j] = a;
-                        a++;
-                    }
-                    b++;
-                    for (i = array.length-2; i >=j+1; i--) {
-                        j = 0;
-                        array[i][j] = a;
-                        a++;
-                    }
-                    b++;
-
-
-                }
-            }
+        if (array[i][j]==0){
+            array[i][j]=a;
         }
 
 
             for (i = 0; i < array.length; i++) {
                 for (j = 0; j < array.length; j++) {
-                    System.out.print(array[i][j] + "  ");
+                    System.out.print(array[i][j] + "    ");
                 }
                 System.out.println();
             }
